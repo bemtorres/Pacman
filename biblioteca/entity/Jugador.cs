646e3327ccdea2f9a9f306098ab13d01332374c2
Benja <benja.mora.torres@gmail.com>
2017-09-16 23:31:58 -0300

    Se agrega proyecto c#

---
+++ biblioteca/entity/Jugador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace biblioteca
+{
+    public class Jugador
+    {
+        public String nombre { get; set; }
+        public int puntaje { get; set; }
+        public int vida { get; set; }
+
+        public Jugador(String nombre, int puntaje) {
+            this.nombre = nombre;
+            this.puntaje = puntaje;          
+        }
+
+
+        public Jugador() {
+            Init();
+        }
+
+        private void Init() {
+            nombre = String.Empty;
+            puntaje = 0;
+            vida = 0;
+        }
+
+        public override string ToString()
+        {
+            return "Nombre jugador: "+nombre+"  score: "+ puntaje;
+        }
+    }
+}
---
+++ biblioteca/entity/JugadorColeccion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace biblioteca.entity
+{
+    public class JugadorColeccion
+    {
+        private List<Jugador> jugadores = new List<Jugador>();
+
+        public JugadorColeccion() {
+            jugadores = new List<Jugador>();
+        }
+
+        public Boolean agregar(Jugador jugador) {
+            if (jugador != null)
+            {
+                jugadores.Add(jugador);
+                return true;
+            }
+            return false;
+        }
+
+        public List<Jugador> listar() {
+            return jugadores;
+        }
+
+        public void llenar() {
+            agregar(new Jugador("aaa",200));
+            agregar(new Jugador("ccc",400));
+            agregar(new Jugador("OOO",500));
+            agregar(new Jugador("Maestro",1000));
+            agregar(new Jugador("mrbm",2000));
+            agregar(new Jugador("win",1));
+        }
+    }
+}
